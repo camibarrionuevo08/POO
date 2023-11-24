26150fd538af27a1bc8b375bc9c937b66b4734cd
camibarrionuevo08 <150205326+camibarrionuevo08@users.noreply.github.com>
2023-11-24 11:48:08 -0500

    Calculadora

---
+++ Calculadora.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadora;
+
+/**
+ *
+ * @author Usuario
+ */
+import java.util.Scanner;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double resultado = 0;
+
+        System.out.println("Bienvenido a la Calculadora");
+
+        System.out.print("Ingrese el primer número: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Ingrese el segundo número: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Seleccione la operación a realizar:");
+        System.out.println("1 - Suma");
+        System.out.println("2 - Resta");
+        System.out.println("3 - Multiplicación");
+        System.out.println("4 - División");
+        int opcion = scanner.nextInt();
+
+        switch (opcion) {
+            case 1:
+                resultado = sumar(num1, num2);
+                break;
+            case 2:
+                resultado = restar(num1, num2);
+                break;
+            case 3:
+                resultado = multiplicar(num1, num2);
+                break;
+            case 4:
+                resultado = dividir(num1, num2);
+                break;
+            default:
+                System.out.println("Opción inválida");
+        }
+
+        System.out.println("El resultado es: " + resultado);
+        scanner.close();
+    }
+
+    public static double sumar(double a, double b) {
+        return a + b;
+    }
+
+    public static double restar(double a, double b) {
+        return a - b;
+    }
+
+    public static double multiplicar(double a, double b) {
+        return a * b;
+    }
+
+    public static double dividir(double a, double b) {
+        if (b != 0) {
+            return a / b;
+        } else {
+            System.out.println("Error: No se puede dividir por cero.");
+            return Double.NaN; // NaN representa "Not a Number"
+        }
+    }
+}
